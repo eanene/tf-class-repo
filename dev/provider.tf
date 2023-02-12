d14f9f3db6177e8a6dd413423b8a63867ece375c
Ebuka Anene <93458489+eanene@users.noreply.github.com>
2023-02-12 13:22:36 -0500

    Update provider.tf

--- dev/provider.tf
+++ dev/provider.tf
@@ -1,20 +1,20 @@
-provider "aws" {
-  region = "us-east-1"
+# provider "aws" {
+#   region = "us-east-1"
 
-  default_tags {
-    tags = {
-      project     = "class-project"
-      environment = var.env
-      owner       = "devops-team"
-    }
-  }
-}
+#   default_tags {
+#     tags = {
+#       project     = "class-project"
+#       environment = var.env
+#       owner       = "devops-team"
+#     }
+#   }
+# }
 
-terraform {
-  backend "s3" {
-    bucket         = "terraform-class-bucket"
-    key            = "class/tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "class-db"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-class-bucket"
+#     key            = "class/tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "class-db"
+#   }
+# }
