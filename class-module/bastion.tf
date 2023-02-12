75247f07c3b6aea462373e5f0476014897f05b23
Ebuka Anene <93458489+eanene@users.noreply.github.com>
2023-02-12 13:16:12 -0500

    Update bastion.tf

--- class-module/bastion.tf
+++ class-module/bastion.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "bastion" {
-  ami           = data.aws_ami.redhat-linux-6.id
-  instance_type = "t2.micro"
+# resource "aws_instance" "bastion" {
+#   ami           = data.aws_ami.redhat-linux-6.id
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "bastion-host"
-  }
-}
+#   tags = {
+#     Name = "bastion-host"
+#   }
+# }
