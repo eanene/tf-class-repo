9e4d9b8e000fd0255ed7ec176640b0f4a696030c
Ebuka Anene <93458489+eanene@users.noreply.github.com>
2023-02-12 13:24:39 -0500

    Update dev.tf

--- dev/dev.tf
+++ dev/dev.tf
@@ -1,11 +1,11 @@
 # tfvars = the values to all the variables in your variable.tf file
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-2"
 }
 
 
 
 module "dev" {
   source = "../class-module/files"
-  cidr_block = " 10.0.0.0/24"
+  cidr_block = " 10.0.0.0/16"
 }
